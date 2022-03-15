9f998756a85b141a2dcc04240f6ddb4413c09ad4
andermatt64 <andermatt64@outlook.com>
2022-03-15 02:23:22 -0400

    local specific changes

--- scripts/postprocess.sh
+++ scripts/postprocess.sh
@@ -5,6 +5,20 @@ if [ "$1" = "" ] || [ "$2" = "" ]; then
     exit 1
 fi
 
-REPO_ROOT=$(realpath "$(dirname ${0})/..")
+REPO_ROOT=$(cd "$(dirname "$0")/.."; pwd -P)
+LOCAL_DIR="${REPO_ROOT}/local"
+POSTPROCESSING_SCRIPT="${LOCAL_DIR}/${1}.local.sh"
 
-echo ${REPO_ROOT}
+if [ -x "${POSTPROCESSING_SCRIPT}" ]; then
+    sh ${POSTPROCESSING_SCRIPT} "${2}"
+    if [ "$?" = "0" ]; then
+        echo "Finished running postprocessing script at ${POSTPROCESSING_SCRIPT}"
+    else
+        echo "Bad exit code when running ${POSTPROCESSING_SCRIPT}"
+        exit 1
+    fi
+else
+    echo "No local postprocessing script to apply for ${1}"
+fi
+
+exit 0
