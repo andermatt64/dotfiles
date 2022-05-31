71dd08dea03361f38898f93b33ed9cb1a7d305b7
andermatt64 <andermatt64@outlook.com>
2022-05-31 01:27:06 -0400

    Fix bad pciom check

--- picom/checks.sh
+++ picom/checks.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-if [ "$(command -v picom 2> /dev/null)" = "" ]; then
+PICOM_BIN=$(command -v picom)
+
+if [ "${PICOM_BIN}" = "" ]; then
+    printf "0"
     exit 1
 fi
 
+printf "1"
 exit 0
+
