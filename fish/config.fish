ebaa65b9f1f6d55c471ee5f38027f4e8e60011fb
andermatt64 <andermatt64@outlook.com>
2023-08-04 19:42:09 -0400

    Add miniforge init

--- fish/config.fish
+++ fish/config.fish
@@ -20,6 +20,9 @@ if status is-interactive
     alias cat="bat"
   end
 
+  if test -f /opt/homebrew/Caskroom/miniforge/base/bin/conda
+      eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
+  end
+
   starship init fish | source
 end
-
