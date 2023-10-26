593c2bfee63a28db8fd23332b2add2075ea0f5f3
andermatt64 <andermatt64@outlook.com>
2023-10-26 19:26:56 -0400

    Update configs

--- fish/config.fish
+++ fish/config.fish
@@ -11,7 +11,7 @@ if status is-interactive
   
   fish_add_path ~/.cargo/bin/
   fish_add_path ~/.local/bin/
-    
+  
   if type -q exa
     alias ls="exa"
   end
@@ -20,6 +20,13 @@ if status is-interactive
     alias cat="bat"
   end
 
+  if test -f /opt/homebrew/opt/ruby/bin/ruby
+    fish_add_path /opt/homebrew/opt/ruby/bin
+    set -gx LDFLAGS "-L/opt/homebrew/opt/ruby/lib"
+    set -gx CPPFLAGS "-I/opt/homebrew/opt/ruby/include"
+    set -gx PKG_CONFIG_PATH "/opt/homebrew/opt/ruby/lib/pkgconfig"
+  end
+  
   if test -f /opt/homebrew/Caskroom/miniforge/base/bin/conda
       eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
   end
