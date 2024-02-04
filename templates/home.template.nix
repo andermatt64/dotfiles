a90575453ff17babd568acda55892a3393f48474
andermatt64 <andermatt64@outlook.com>
2024-02-04 00:48:49 -0500

    Fix themes

--- templates/home.template.nix
+++ templates/home.template.nix
@@ -49,7 +49,6 @@
       cat = "bat";
     };
     interactiveShellInit = ''
-      starship init fish | source
     '';
     shellInit = ''
       if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
@@ -88,6 +87,7 @@
           owner = "catppuccin";
           repo = "bat";
           rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+          sha256 = "6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
         };
         file = "Catppuccin-macchiato.tmTheme";
       };
