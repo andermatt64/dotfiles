abeb8b5599ce762a88a93ac387338a5c5018b865
Andy <andermatt64@outlook.com>
2024-06-14 23:53:31 -0400

    Switch to fish (#4)
    * Remove nushell for fish
    
    * zellij uses fish now
    
    * remove skim fish integration

--- templates/home.template.nix
+++ templates/home.template.nix
@@ -34,14 +34,6 @@
   home.file = {
     ".config/wezterm/wezterm.lua".source = ./wezterm/config.lua;
     ".config/starship.toml".source = ./starship/config.toml;
-
-    # TODO: when carapace improves quoting of file paths with spaces, remove these
-    ".config/nushell/cargo-completions.nu".source = ./nushell/cargo-completions.nu;
-    ".config/nushell/git-completions.nu".source = ./nushell/git-completions.nu;
-    ".config/nushell/make-completions.nu".source = ./nushell/make-completion.nu;
-    ".config/nushell/nix-completions.nu".source = ./nushell/nix-completions.nu;
-    ".config/nushell/yarn-v4-completions.nu".source = ./nushell/yarn-v4-completions.nu;
-    ".config/nushell/zellij-completions.nu".source = ./nushell/zellij-completions.nu;
   } 
     // (
       if osName == "darwin" then darwinCfg.files 
@@ -184,104 +176,44 @@
       ];
     };
   };
-  programs.nushell = {
+  programs.fish = {
     enable = true;
 
-    envFile.text = ''
-      $env.ENV_CONVERSIONS = {
-        "PATH": {
-          from_string: { |s| $s | split row (char esep) | path expand --no-symlink }
-          to_string: { |v| $v | path expand --no-symlink | str join (char esep) }
-        }
-        "Path": {
-          from_string: { |s| $s | split row (char esep) | path expand --no-symlink }
-          to_string: { |v| $v | path expand --no-symlink | str join (char esep) }
-        }
-      };
-    '';
-    
-    configFile.text = ''
-      $env.config = {
-        show_banner: false,
-      };
-    '';
-    
-    environmentVariables = {
-      EDITOR = "hx";
-      VISUAL = "hx";
-    };
-
-    extraConfig = ''
-      def nuopen [arg, --raw (-r)] { if $raw { open -r $arg } else { open $arg } }
-      def fenv [cmd] {
-        mut new_env = {};
-  
-        let cmdline = $"source ($cmd) && env -0";
-        let raw_env = (bash -c $cmdline | encode utf8);
-        let delims = ($raw_env | bytes index-of --all 0x[00]);
-
-        mut prev_delim = 0;
-        for delim in $delims {
-          let row = ($raw_env | bytes at $prev_delim..$delim | decode utf8 | split column "=");
-
-          $prev_delim = $delim + 1;
-
-          let key = ($row | get column1 | first);
-          mut val = ($row | get column2 | first);
-
-          if $key in [_ SHLVL PWD PROMPT_MULTILINE_INDICATOR PROMPT_INDICATOR DIRS_POSITION LAST_EXIT_CODE DIRENV_DIFF DIRENV_WATCHES SHELL NIX_CFLAGS_COMPILE] {
-            continue;
-          }
-
-          if $key =~ '^BASH_.*$' or $key =~ '^NU_.*$' {
-            continue;
-          }
-
-          if $key in $env.ENV_CONVERSIONS {
-            $val = (do $env.ENV_CONVERSIONS.PATH.from_string $val);
-          }
-    
-          if ($key not-in $env) or ($env | get $key) != $val {
-            $new_env = ($new_env | insert $key $val);
-          }
-        }
-
-        $new_env
+    plugins = [
+      {
+        name = "foreign-env";
+        src = pkgs.fetchFromGitHub {
+          owner = "oh-my-fish";
+          repo = "plugin-foreign-env";
+          rev = "7f0cf099ae1e1e4ab38f46350ed6757d54471de7";
+          sha256 = "4+k5rSoxkTtYFh/lEjhRkVYa2S4KEzJ/IJbyJl+rJjQ=";
+        };
       }
-
-      if ('/opt/homebrew/bin' | path exists) {
-        $env.PATH = ($env.PATH | prepend '/opt/homebrew/bin');
-      }
-
-      if ('~/.cargo/bin' | path exists) {
-        $env.PATH = ($env.PATH | append `~/.cargo/bin`);
-      }
-
-      if ('~/.local/bin' | path exists) {
-        $env.PATH = ($env.PATH | append '~/.local/bin');
-      }
-
-      if ('/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' | path exists) {
-        fenv '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' | load-env;
-      }
-
-      # TODO: remove when carapace is better with paths with spaces  
-      source ~/.config/nushell/cargo-completions.nu
-      source ~/.config/nushell/git-completions.nu
-      source ~/.config/nushell/make-completions.nu
-      source ~/.config/nushell/nix-completions.nu
-      source ~/.config/nushell/yarn-v4-completions.nu
-      source ~/.config/nushell/zellij-completions.nu
-    '';
-
+    ];
     shellAliases = {
       cat = "bat";
-      la = "eza -a";
-      ll = "eza -l";
-      lla = "eza -la";
-      lt = "eza --tree";
-      open = "^open";
     };
+    interactiveShellInit = ''
+      set -x EDITOR hx
+      set -x VISUAL hx
+      
+      if test -d /opt/homebrew/bin
+        fish_add_path /opt/homebrew/bin
+      end
+
+      if test -d ~/.cargo/bin
+        fish_add_path ~/.cargo/bin
+      end
+
+      if test -d ~/.local/bin
+        fish_add_path ~/.local/bin
+      end
+    '';
+    shellInit = ''
+      if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+        fenv source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+      end
+    '';
   };
   programs.btop = {
     enable = true;
@@ -290,12 +222,17 @@
       color_theme = "onedark";
     };
   };
+  programs.skim = {
+    enable = true;
+
+    enableFishIntegration = false;
+  };
   programs.zellij = {
     enable = true;
 
     settings = {
       theme = "catppuccin-macchiato";
-      default_shell = "nu";
+      default_shell = "fish";
       pane_frames = false;
       keybinds = {
         pane = {
